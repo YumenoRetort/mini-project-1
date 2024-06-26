5e048c6b2f79876bb481496a64a779e4a9e875b8
Erin Beatrice Micaela G. Reyes <89332224+YumenoRetort@users.noreply.github.com>
2024-06-26 11:38:19 +0800

    Update Main.java

--- Mini-Project-3/src/main/java/org/example/Main.java
+++ Mini-Project-3/src/main/java/org/example/Main.java
@@ -1,7 +1,54 @@
+/**
+ * This class is the main class of the program.
+ * The program is a simple library management system
+ * that allows the user to view, add, remove, and search
+ * for library items (limited to books at this time).
+ *
+ * @author Erin Beatrice Micaela G. Reyes
+ */
+
 package org.example;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner myScan = new Scanner(System.in);
+        String loop;
+
+        System.out.print("Hello! Welcome to the supermarket! \n");
+
+        do {
+            loop = ""; // Reset loop
+
+            // Main Menu
+            try {
+                System.out.print("\nSelect a Function: \nView Books[1], \nAdd a Book [2], \nDelete a Book [3], \nSearch for a Book [4] \n");
+                int optionInput = myScan.nextInt();
+                myScan.nextLine(); // Consume newline character left in the buffer
+
+
+            } catch (InputMismatchException e) { // Error Handling
+                System.out.println("\nAn error occurred Invalid input. Please enter a number.\n");
+                myScan.nextLine(); // Clear the scanner buffer
+
+            } catch (Exception e) {
+                System.out.println("\nAn error occurred: " + e.getMessage() + "\n");
+                myScan.nextLine(); // Clear the scanner buffer
+            }
+
+            // Loop Menu
+            while(!loop.equalsIgnoreCase("Y") && !loop.equalsIgnoreCase("N")){
+                System.out.println("Return to the main menu? Y/N?");
+                loop = myScan.nextLine().trim();
+
+                if(!loop.matches("[yYnN]")) {
+                    System.out.println("\nInvalid input. Please type Y/N.");
+                }
+            }
+        } while(loop.equalsIgnoreCase("Y"));
+
+        myScan.close();
     }
 }
